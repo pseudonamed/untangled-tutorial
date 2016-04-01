8e520c67f74da14eb72bb895ab4d8106149f7a08
Donavan-Ross Costaras <d.costaras@gmail.com>
2016-04-01 08:10:08 +0200

    App db exercises

--- src/tutorial/untangled_tutorial/app_database/exercises.cljs
+++ src/tutorial/untangled_tutorial/app_database/exercises.cljs
@@ -1,17 +1,18 @@
 (ns untangled-tutorial.app-database.exercises)
 
 (def cars-table
-  {
-   ; TODO (exercise 1): Add a :cars/by-id table
-   })
+  {:cars/by-id {1 { :id 1 :make "Nissan" :model "Leaf" }
+                2 { :id 2 :make "Dodge" :model "Dart" }
+                3 { :id 3 :make "Ford" :model "Mustang" }}})
 
 (def favorites
-  ; TODO (exercise 2): merge your cars table from above here
-  {
-   ; TODO (exercise 2): Add a :favorite-car key that points to the Nissan Leaf via an ident
-   })
+  (merge cars-table {:favorite-car [:cars/by-id 1]}))
 
 (def ex3-uidb
-  {
-   ; TODO (exercise 3): Add tables. See exercise text.
-   })
+  {:tools/by-id {1 {:id 1 :label "Cut"}
+                 2 {:id 2 :label "Copy"}}
+   :data/by-id {5 {:id 5 :x 1 :y 3}}
+   :toolbar {:main {:tools [[:tools/by-id 1] [:tools/by-id 2]]}}
+   :canvas {:main {:data [[:data/by-id 5]]}}
+   :main-panel {:toolbar [:toolbar :main]
+                :canvas [:canvas :main]}})
